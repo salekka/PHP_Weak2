7d02e580b5143743de1c251f3d460ce3aca6fe6b
Stanislav <salekkaexe@gmail.com>
2024-11-25 10:46:39 +0400

    Update index.php

--- index.php
+++ index.php
@@ -2,33 +2,44 @@
 session_start();
 include 'storage.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// Проверяем, был ли отправлен POST-запрос (т.е. форма была отправлена)
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+{
+    // Получаем значения из POST-запроса, если они установлены, иначе устанавливаем пустую строку
     $username = $_POST['username'] ?? '';
     $password = $_POST['password'] ?? '';
     $feedback = $_POST['feedback'] ?? '';
     
+    // Массив для хранения ошибок валидации
     $errors = [];
     
-    if (empty($username)) {
+    if (empty($username)) 
+    {
         $errors[] = "Необходимо ввести имя пользователя";
     }
-    if (empty($password)) {
+    if (empty($password)) 
+    {
         $errors[] = "Необходимо ввести пароль";
     }
-    if (empty($feedback)) {
+    if (empty($feedback)) 
+    {
         $errors[] = "Необходимо оставить отзыв";
     }
     
-    if (empty($errors)) {
-        $user = null;
-        foreach ($users as $u) {
-            if ($u['username'] === $username && $u['password'] === $password) {
+    if (empty($errors))
+    {
+        $user = null;// Переменная для хранения найденного пользователя
+        foreach ($users as $u)// Ищем пользователя в массиве $users по имени пользователя и паролю
+            {
+            if ($u['username'] === $username && $u['password'] === $password)
+            {
                 $user = $u;
                 break;
             }
         }
         
-        if ($user) {
+        if ($user) 
+        {
             $_SESSION['user'] = $user;
             $_SESSION['login_time'] = date('Y-m-d H:i:s');
             $_SESSION['feedback'] = $feedback;
@@ -38,7 +49,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             
             header('Location: about.php');
             exit;
-        } else {
+        } 
+        else 
+        {
             $errors[] = "Неверное имя пользователя или пароль";
         }
     }
